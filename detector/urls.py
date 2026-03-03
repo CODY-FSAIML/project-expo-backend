f4ac6879007e75cada263b0f0a4e763d9581dc6a
23981a05ae@raghuenggcollege.in <23981a05ae@raghuenggcollege.in>
2026-03-03 19:33:22 +0530

    render fixing 2

--- detector/urls.py
+++ detector/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/analyze-text/', views.analyze_text, name='analyze_text'),
-    path('api/analyze-audio/', views.analyze_audio, name='analyze_audio'),
-    path('api/analyze-video/', views.analyze_video, name='analyze_video'),
+    path('api/analyze-text', views.analyze_text, name='analyze_text'),
+    path('api/analyze-audio', views.analyze_audio, name='analyze_audio'),
+    path('api/analyze-video', views.analyze_video, name='analyze_video'),
 ]
